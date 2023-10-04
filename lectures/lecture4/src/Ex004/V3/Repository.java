780c49161e92b6d0a7e2cdb6027286296936495b
Aynur Shamsullin <lilmoon99@ya.ru>
2023-10-04 21:33:47 +0400

    lecture 4 added

---
+++ lectures/lecture4/src/Ex004/V3/Repository.java
@@ -0,0 +1,36 @@
+package Ex004.V3;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Repository<T extends Content> {
+    List<T> ds;
+
+    private String name;
+
+    public Repository(String name) {
+        this.ds = new ArrayList<>();
+        this.name = name;
+        System.out.printf("\n  >>  %s created\n",this.name);
+    }
+
+    public void add(T content) {
+        ds.add(content);
+    }
+
+    public int count() {
+        return ds.size();
+    }
+
+    public T get(Integer index) {
+        return ds.get(index);
+    }
+
+    // В качестве тренировки реализуйте возможность работы с foreach
+    public void printAll(List<T> list){
+        for (T el: list){
+            System.out.println(el);
+        }
+    }
+}
